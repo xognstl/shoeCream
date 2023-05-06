4496de2051523bbebead5cae0593e6f89242cd67
xognstl <xognstl@naver.com>
2023-05-06 17:38:34 +0900

    Initial commit

---
+++ src/main/java/shoecream/ShoeCreamApplication.java
@@ -0,0 +1,13 @@
+package shoecream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoeCreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoeCreamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/shoecream/ShoeCreamApplicationTests.java
@@ -0,0 +1,13 @@
+package shoecream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoeCreamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
